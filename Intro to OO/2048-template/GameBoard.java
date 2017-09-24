ffc5d2e3db2956a15c2f79eaed2f62235a576f95
Chris Brooks-Prenger <brooksprench@hdsb.ca>
2017-09-24 18:15:04 -0400

    Adding 2048 Template

---
+++ Intro to OO/2048-template/GameBoard.java
@@ -0,0 +1,118 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameBoard extends World
+{
+
+    //Instance Constants
+    
+    /* Note: 
+     * Since these variables are definied at STATIC they will never change (aka are constant).
+     * Normally when we have constant variables like this we name them in ALL CAPS
+    */
+    //Define some directions to make it easier to reference which way the blocks are moving
+    private static final int UP = 0;
+    private static final int RIGHT = 1;
+    private static final int DOWN = 2;
+    private static final int LEFT = 3;
+    
+    
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public GameBoard()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(4, 4, 50); 
+        
+        //populate gameboard with x randomly placed objects
+        
+    }
+    
+    
+    /**
+     * Place a block on a random location on the board
+     * 
+     * @return Returns true if successful, false if not successful
+     */
+    private boolean placeRandomBlock()
+    {
+        //Generate Random Location
+        
+        //Check to ensure random location is not yet taken, if the spot is free add it to the world
+        
+        return false;
+    }
+    
+    /**
+     * Act - Check for key presses and tell each block to move itsself.
+     */
+    public void act() 
+    {
+        //Add key press actoins here
+        String key = Greenfoot.getKey();
+        
+        //If a key was pressed...do something
+        if (key != null) {
+            
+            //Note: you should disable this, but I wanted to show how you can debug in greenfoot
+            System.out.println(key);  
+            
+            switch(key) {
+                case "up": 
+                    //Tell the blocks to move up
+                    //Start checking from the top, then move downwards
+
+                    for (int i =0; i< getWidth(); i++) {
+                        for (int j=0; j<getHeight(); j++) {
+                            
+                            List blockList = getObjectsAt(i,j,GameSquare.class);
+            
+                            //Tell the other block object we wish to merge with it.  If successful, delete this block from the game
+                            if (blockList.size() == 1) { //Error checking
+                                //Get the first (and only) entry in the list
+                                //Convert it from the generic "Object" to a GameSquare Object
+                                //Then move UP.  All the following was accomplished on the single line below
+                                //It can be a bit confusing when code is all in one line.  Is this considered good form or bad form?
+                                ( (GameSquare)( blockList.get(0) )).move(UP);  
+                            }
+                        }
+                    }
+                    break;
+                //NOTE: The remaining cases are similar to the one above, but not exactly the same
+                case "right":
+                    //Tell the blocks to move right
+                    //Start checking from the right most col, then move left                    
+                    break;
+
+                case "down":
+                    //Tell the blocks to move down
+                    //Start checking from the bottom, then move up
+                    break;
+
+                case "left":
+                    //Tell the blocks to move left
+                    //Start checking from the left, then move right
+                    break;
+
+                }
+                
+
+            //Since placeRandomBlock is not gaurenteed to work the first time, repeat the process until it does
+            int count = 0;
+            while (count < 1) {
+                if (placeRandomBlock()) {
+                    count++;
+                }
+            }            
+        }
+        
+    }
+}
---
+++ Intro to OO/2048-template/GameSquare.java
@@ -0,0 +1,143 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+
+/**
+ * Write a description of class GameSquare here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameSquare extends Actor
+{
+    //Instance Constants
+    
+    /* Note: 
+     * Since these variables are definied at STATIC they will never change (aka are constant).
+     * Normally when we have constant variables like this we name them in ALL CAPS
+    */
+    //Define some directions to make it easier to reference which way the blocks are moving
+    private static final int UP = 0;
+    private static final int RIGHT = 1;
+    private static final int DOWN = 2;
+    private static final int LEFT = 3;
+    
+    //Instance Variables    
+    private boolean firstRun = true;
+    private int value = 2;
+    private boolean debug=false;
+    
+        
+    //Constructor
+    public GameSquare()
+    {
+        displayValue();
+    }
+    
+    /**
+     * Tell the block to move in the given direction until it hits an obstacle
+     * 
+     * @param The direction in which the block is to move (UP = 0; RIGHT = 1; DOWN = 2; LEFT = 3;
+     */
+    public void move(int direction) 
+    {
+        //check if can move
+        int movable = canMove(direction);
+
+        //if moveable, start a loop
+        while (movable > 0)
+        {
+            //Get current coordinates
+            //ADD CODE
+            
+            //Change x and y values to the "new" location based on direction
+            //ADD CODE
+            
+            //If Nothing in the way - move the block
+            if (movable == 1) {  
+                setLocation(x,y);
+                movable = canMove(direction);
+                continue;
+                //return;
+            }
+            //Merge the blocks
+            else {
+                //Find which block we need to merge with
+                //ADD CODE
+                
+                //Tell the other block object we wish to merge with it.  If successful, delete this block from the game
+                //ADD CODE
+                
+                
+                
+                
+                return;
+            }
+        }
+        
+        
+        //can't move, so don't move.
+        return;
+    }
+
+    /**
+     * Merge with another block and combine values.
+     * Will only work if the two blocks are of the same value
+     * 
+     * @param The value of the block to be merged with
+     * 
+     * @return Return true if the merge is successful.
+     */
+    public boolean merge(int valueIn)
+    {
+        //Insert Code
+    }
+    
+    /**
+     * Returns the current value of the gameSquare
+     * 
+     * @return The current value (int) of the game square
+     */
+    
+    public int getValue()
+    {
+        //ADD CODE
+    }
+
+    /**
+     * Checks to see if the block can move
+     * 
+     * @return int value representing what is in the space to be moved to.  0 -> Path Blocked, 1 -> Empty Space, int>1 value of block in the space to be moved to.
+     */
+    private int canMove(int direction)
+    {
+        //Get World
+        //ADD CODE
+        
+        //Get x and y values of current object
+        //ADD CODE        
+        
+        //Change x and y values to the "new" location based on direction
+        
+        //Test for outside border
+           
+        
+        //Check to see if there is a block in the way
+        
+        
+        return 1;
+    }
+    
+    /**
+     * displayValue - Displays the current value of a block in an image, then sets that image to the block display image
+     */    
+    private void displayValue() 
+    {
+        //Create an image consisting of the display value using built in greenfoot commands
+        GreenfootImage displayImage;
+        displayImage = new GreenfootImage( Integer.toString(value), 20, Color.BLACK, Color.WHITE);
+        
+        //Add the image as the new image for this object
+        setImage(displayImage);
+        
+    }
+}
